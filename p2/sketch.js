53ff70425faf866954fd0fb164b81862ba0ae815
nicoleyuen <38094767+nicoleyuen@users.noreply.github.com>
2019-05-07 14:19:27 -0700

    Update sketch.js

--- p2/sketch.js
+++ p2/sketch.js
@@ -1,7 +1,7 @@
 //https://www.youtube.com/watch?v=S1TQCi9axzg
 
 var streams = [];
-var symbolSize = 55;
+var symbolSize = 35;
 // this is a global variable to make the character/ text size bigger
 
 function setup() {
